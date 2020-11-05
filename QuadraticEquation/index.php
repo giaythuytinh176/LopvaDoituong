e116f5be674b2e55478dea2a580c8cddd55b43d3
LE DUC TAM <giaythuytinh176@gmail.com>
2020-11-05 14:28:48 +0700

    update

--- QuadraticEquation/index.php
+++ QuadraticEquation/index.php
@@ -2,15 +2,15 @@
 
 require "QuadraticEquation.php";
 
-$solveEqua = new QuadraticEquation(10, 44, 44);
+$solveEqua = new QuadraticEquation(1, -5, 4);
 
 if ($solveEqua->getDiscriminant() > 0) {
 
-    echo "pt co nghiem x1=" . $solveEqua->getRoot1() . "<br>";
-    echo "pt co nghiem x2=" . $solveEqua->getRoot2();
+    echo "pt co nghiem x1 = " . $solveEqua->getRoot1() . "<br>";
+    echo "pt co nghiem x2 = " . $solveEqua->getRoot2();
 
 } elseif ($solveEqua->getDiscriminant() == 0) {
-    echo "pt co nghiem x1=x2=" . $solveEqua->getRoot1();
+    echo "pt co nghiem x1 = x2 = " . $solveEqua->getRoot1();
 } else {
     echo "pt vo nghiem";
 }
